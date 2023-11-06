6c790c404dd5668cbacfe189a6bf5c74d8a5c270
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-11-06 13:39:01 +0100

    Курс 3 Домашнее задание 37 Миграции и индексы изначальный комментарий

---
+++ src/main/java/sky/pro/course3/homework/Course3Homework37Application.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Homework37Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3Homework37Application.class, args);
+    }
+
+}
